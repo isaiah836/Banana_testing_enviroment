d374dbfb7263a5f284bfe78595795e74da111a8e
ISAIAH SANCHEZ <ISASANCH@uat.edu>
2018-06-19 16:38:19 -0700

    Add project files.

---
+++ Banana_testing_enviroment/People.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "stdafx.h"
+
+using namespace std;
+
+//declaring functions
+void Delores(string);
+void Arnold(string);
---
+++ Banana_testing_enviroment/People_s.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "People.h"
+
+void Delores(string textToSpeak)
+{
+	cout << textToSpeak << endl;
+	return;
+}
+
+void Arnold(string textToSpeak)
+{
+	cout << textToSpeak << endl;
+	return;
+}
